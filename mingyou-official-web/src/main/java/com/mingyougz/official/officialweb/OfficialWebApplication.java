f5ead6090fd484e3a804879d2fc5b029094253ac
zrda27@163.com <da901213>
2017-06-12 23:48:57 +0800

    123

---
+++ mingyou-official-web/src/main/java/com/mingyougz/official/officialweb/OfficialWebApplication.java
@@ -0,0 +1,12 @@
+package com.mingyougz.official.officialweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficialWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficialWebApplication.class, args);
+	}
+}
